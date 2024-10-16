fea1df3670a3af9cb5c11311cd96a3cccfa1eaa3
Bad Sparrow <yadavrahul9966@gmail.com>
2024-10-16 13:19:57 -0400

    improved code for the spider

--- shoescrapper/shoescrapper/spiders/uaspider.py
+++ shoescrapper/shoescrapper/spiders/uaspider.py
@@ -1,20 +1,29 @@
 import scrapy
+from shoescrapper.items import ShoescrapperItem
+from scrapy.loader import ItemLoader
 
 
 class UarmourSpider(scrapy.Spider):
     name = 'armour'
     start_urls = ['https://www.underarmour.ca/en-ca/c/mens/shoes/']
 
-    def parse(self, response):
-        
-        base_url = "https://www.underarmour.ca"
+    def parse(self, response):       
+        base_url = 'https://www.underarmour.ca'
         
         for products in response.css('div.false.ProductTile_product-tile-container__flx2K.module__primary-img.false.ProductTile_split-view-disabled__3P8wI'):
-            yield {
-                'name': products.css('a.ProductTile_product-item-link__tSc19::text').get(),
-                'price': products.css('span.bfx-price.bfx-list-price::text').get().replace('$', ''),
-                'link': base_url + products.css('a.ProductTile_product-item-link__tSc19').attrib['href'],
-            }
+            il = ItemLoader(item = ShoescrapperItem(), selector= products)
+
+            il.add_css('name', 'a.ProductTile_product-item-link__tSc19')
+            il.add_css('price', 'span.bfx-price.bfx-list-price')
+            
+            relative_url = products.css('a.ProductTile_product-item-link__tSc19::attr(href)').get()
+            
+            if relative_url:    
+                il.add_value('link', base_url + relative_url)
+
+            yield il.load_item()
+            
+        
 
         next_page = base_url + response.css('a.link_underline').attrib['href']
 
